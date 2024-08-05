ae396e9140a605526e110eb3e1925ffa9d70f0b0
unknown <malavikacheeran@gmail.com>
2024-08-06 00:58:34 +0530

    sum.java added

---
+++ sum.java
@@ -0,0 +1,13 @@
+package USTAssignment;
+
+public class Sum{
+
+    public static void main(String[] args){
+        int a, b, sum;
+        a=100;
+        b=200;
+        sum=a+b;
+        System.out.println("Sum:" +sum);
+
+    }
+}
